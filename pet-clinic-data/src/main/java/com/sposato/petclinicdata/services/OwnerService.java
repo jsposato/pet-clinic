21e3ea332dfb24f36c430a2d0dbc44efafcb2a00
jsposato <jsposato@gmail.com>
2019-10-25 11:51:11 -0400

    Refactor services with a base Crud service

--- pet-clinic-data/src/main/java/com/sposato/petclinicdata/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/sposato/petclinicdata/services/OwnerService.java
@@ -1,15 +1,8 @@
 package com.sposato.petclinicdata.services;
 
 import com.sposato.petclinicdata.model.Owner;
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/sposato/petclinicdata/services/PetService.java
+++ pet-clinic-data/src/main/java/com/sposato/petclinicdata/services/PetService.java
@@ -2,14 +2,6 @@ package com.sposato.petclinicdata.services;
 
 import com.sposato.petclinicdata.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
